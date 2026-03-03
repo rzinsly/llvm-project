a0bc87ac85974448b1baa0be5357b54d70d7cee6
Raphael Moreira Zinsly <raphael.zinsly@oss.qualcomm.com>
2026-03-03 14:38:30 -0300

    WIP LAA set ShouldRetryWithRuntimeChecks in getDependenceDistanceStrideAndSize

--- llvm/lib/Analysis/LoopAccessAnalysis.cpp
+++ llvm/lib/Analysis/LoopAccessAnalysis.cpp
@@ -2121,6 +2121,21 @@ MemoryDepChecker::getDependenceDistanceStrideAndSize(
   int64_t StrideBPtrInt = *StrideBPtr;
   LLVM_DEBUG(dbgs() << "LAA:  Src induction step: " << StrideAPtrInt
                     << " Sink induction step: " << StrideBPtrInt << "\n");
+
+  if (!StrideAPtrInt && !StrideBPtrInt && !(AIsWrite && BIsWrite) &&
+      (AIsWrite || BIsWrite) && !isa<UndefValue>(APtr) &&
+      InnermostLoop->isLoopInvariant(APtr) &&
+      InnermostLoop->isLoopInvariant(BPtr)) {
+    LoadInst *L = dyn_cast<LoadInst>(AIsWrite ? BInst : AInst);
+    StoreInst *S = dyn_cast<StoreInst>(AIsWrite ? AInst : BInst);
+    if (L && S && InnermostLoop->isLoopInvariant(L->getPointerOperand()))
+      if (AA->alias(MemoryLocation::get(L), MemoryLocation::get(S)) ==
+          AliasResult::MustAlias)
+        ShouldRetryWithRuntimeChecks = true;
+
+    return MemoryDepChecker::Dependence::Unknown;
+  }
+
   // At least Src or Sink are loop invariant and the other is strided or
   // invariant. We can generate a runtime check to disambiguate the accesses.
   if (!StrideAPtrInt || !StrideBPtrInt)
